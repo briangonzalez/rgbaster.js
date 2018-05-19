afd4c441988504e1dc90dde92b7b586c5e8db87f
Elton Mesquita <callmeelton@gmail.com>
2018-05-19 20:22:49 -0300

    Switched to Rollup and added UMD support
    Fixes #32 and #34. Removed Gulp and added Rollup as it's best suited for
    libraries. Added UMD support for modules and changed the entry point to
    rgbaster.min.js so it can be used as a node module and be successfully
    used with webpack, rollup or any module budler.

--- rgbaster.js
+++ rgbaster.js
@@ -1,120 +1,116 @@
-;(function(window, undefined){
+"use strict";
 
-  "use strict";
+// Helper functions.
+var getContext = function(width, height){
+  var canvas = document.createElement("canvas");
+  canvas.setAttribute('width', width);
+  canvas.setAttribute('height', height);  
+  return canvas.getContext('2d');
+};
 
-  // Helper functions.
-  var getContext = function(width, height){
-    var canvas = document.createElement("canvas");
-    canvas.setAttribute('width', width);
-    canvas.setAttribute('height', height);  
-    return canvas.getContext('2d');
+var getImageData = function(img, loaded){
+
+  var imgObj = new Image();
+  var imgSrc = img.src || img;
+
+  // Can't set cross origin to be anonymous for data url's
+  // https://github.com/mrdoob/three.js/issues/1305
+  if ( imgSrc.substring(0,5) !== 'data:' )
+    imgObj.crossOrigin = "Anonymous";
+
+  imgObj.onload = function(){
+    var context = getContext(imgObj.width, imgObj.height);
+    context.drawImage(imgObj, 0, 0);
+
+    var imageData = context.getImageData(0, 0, imgObj.width, imgObj.height);
+    loaded && loaded(imageData.data);
   };
 
-  var getImageData = function(img, loaded){
+  imgObj.src = imgSrc;
 
-    var imgObj = new Image();
-    var imgSrc = img.src || img;
+};
 
-    // Can't set cross origin to be anonymous for data url's
-    // https://github.com/mrdoob/three.js/issues/1305
-    if ( imgSrc.substring(0,5) !== 'data:' )
-      imgObj.crossOrigin = "Anonymous";
+var makeRGB = function(name){
+  return ['rgb(', name, ')'].join('');
+};
 
-    imgObj.onload = function(){
-      var context = getContext(imgObj.width, imgObj.height);
-      context.drawImage(imgObj, 0, 0);
+var mapPalette = function(palette){
+  var arr = [];
+  for (var prop in palette) { arr.push( frmtPobj(prop, palette[prop]) ) };
+  arr.sort(function(a, b) { return (b.count - a.count) });
+  return arr;
+};  
 
-      var imageData = context.getImageData(0, 0, imgObj.width, imgObj.height);
-      loaded && loaded(imageData.data);
-    };
+var fitPalette = function(arr, fitSize) {
+  if (arr.length > fitSize ) {
+  return arr.slice(0,fitSize);
+} else {
+  for (var i = arr.length-1 ; i < fitSize-1; i++) { arr.push( frmtPobj('0,0,0', 0) ) };
+  return arr;
+};
+};
 
-    imgObj.src = imgSrc;
-
-  };
-
-  var makeRGB = function(name){
-    return ['rgb(', name, ')'].join('');
-  };
-
-  var mapPalette = function(palette){
-    var arr = [];
-    for (var prop in palette) { arr.push( frmtPobj(prop, palette[prop]) ) };
-    arr.sort(function(a, b) { return (b.count - a.count) });
-    return arr;
-  };  
-  
-  var fitPalette = function(arr, fitSize) {
-    if (arr.length > fitSize ) {
-    return arr.slice(0,fitSize);
-  } else {
-    for (var i = arr.length-1 ; i < fitSize-1; i++) { arr.push( frmtPobj('0,0,0', 0) ) };
-    return arr;
-  };
-  };
-  
-  var frmtPobj = function(a,b){
-    return {name: makeRGB(a), count: b};
-  }
+var frmtPobj = function(a,b){
+  return {name: makeRGB(a), count: b};
+}
 
 
-  // RGBaster Object
-  // ---------------
-  //
-  var PALETTESIZE = 10;
+// RGBaster Object
+// ---------------
+//
+var PALETTESIZE = 10;
 
-  var RGBaster = {};
+var RGBaster = {};
 
-  RGBaster.colors = function(img, opts){
+RGBaster.colors = function(img, opts){
 
-    opts = opts || {};
-    var exclude = opts.exclude || [ ], // for example, to exclude white and black:  [ '0,0,0', '255,255,255' ]
-        paletteSize = opts.paletteSize || PALETTESIZE;
+  opts = opts || {};
+  var exclude = opts.exclude || [ ], // for example, to exclude white and black:  [ '0,0,0', '255,255,255' ]
+      paletteSize = opts.paletteSize || PALETTESIZE;
 
-    getImageData(img, function(data){
+  getImageData(img, function(data){
 
-              var colorCounts   = {},
-                  rgbString     = '',
-                  rgb           = [],
-                  colors        = {
-                    dominant: { name: '', count: 0 },
-                    palette:  []
-                  };
+            var colorCounts   = {},
+                rgbString     = '',
+                rgb           = [],
+                colors        = {
+                  dominant: { name: '', count: 0 },
+                  palette:  []
+                };
 
-              var i = 0;
-              for (; i < data.length; i += 4) {
-                rgb[0] = data[i];
-                rgb[1] = data[i+1];
-                rgb[2] = data[i+2];
-                rgbString = rgb.join(",");
+            var i = 0;
+            for (; i < data.length; i += 4) {
+              rgb[0] = data[i];
+              rgb[1] = data[i+1];
+              rgb[2] = data[i+2];
+              rgbString = rgb.join(",");
 
-                // skip undefined data and transparent pixels
-                if (rgb.indexOf(undefined) !== -1  || data[i + 3] === 0) {
-                  continue;
-                }
-
-                // Ignore those colors in the exclude list.
-                if ( exclude.indexOf( makeRGB(rgbString) ) === -1 ) {
-                  if ( rgbString in colorCounts ) {
-                    colorCounts[rgbString] = colorCounts[rgbString] + 1;
-                  }
-                  else{
-                    colorCounts[rgbString] = 1;
-                  }
-                }
-
+              // skip undefined data and transparent pixels
+              if (rgb.indexOf(undefined) !== -1  || data[i + 3] === 0) {
+                continue;
               }
 
-              if ( opts.success ) {
-                var palette = fitPalette( mapPalette(colorCounts), paletteSize+1 );
-                opts.success({
-                  dominant: palette[0].name,
-                  secondary: palette[1].name,
-                  palette:  palette.map(function(c){ return c.name; }).slice(1)
-                });
+              // Ignore those colors in the exclude list.
+              if ( exclude.indexOf( makeRGB(rgbString) ) === -1 ) {
+                if ( rgbString in colorCounts ) {
+                  colorCounts[rgbString] = colorCounts[rgbString] + 1;
+                }
+                else{
+                  colorCounts[rgbString] = 1;
+                }
               }
-    });
-  };
 
-  window.RGBaster = window.RGBaster || RGBaster;
+            }
 
-})(window);
+            if ( opts.success ) {
+              var palette = fitPalette( mapPalette(colorCounts), paletteSize+1 );
+              opts.success({
+                dominant: palette[0].name,
+                secondary: palette[1].name,
+                palette:  palette.map(function(c){ return c.name; }).slice(1)
+              });
+            }
+  });
+};
+
+export default RGBaster

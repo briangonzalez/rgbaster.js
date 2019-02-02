d470d77cc5cbb1498b69588ca2778b6317a147fe
Brian Gonzalez <me@briangonzalez.org>
2019-02-02 08:07:39 -0800

    jest mods

--- jest.config.js
+++ jest.config.js
@@ -3,5 +3,6 @@ module.exports = {
   runner: '@jest-runner/electron',
   testEnvironment: '@jest-runner/electron/environment',
   globalSetup: './tests/global-setup.ts',
-  globalTeardown: './tests/global-teardown.ts'
+  globalTeardown: './tests/global-teardown.ts',
+  testMatch: ["**/__tests__/**/*.ts?(x)", "**/?(*.)+(spec|test).ts?(x)"]
 };
